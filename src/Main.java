d69e881c309e91b881f5db07816bafa3b70b6bef
choiyongbum <72883647+choiyongbum@users.noreply.github.com>
2026-06-22 16:16:50 +0900

    refactor: Main Category 기반 데이터 초기화로 변경 (STEP 3)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import domain.Category;
 import domain.Product;
 import service.CommerceSystem;
 
@@ -7,13 +8,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List<Product> products = new ArrayList<>();
-        products.add(new Product("Galaxy S25", 1200000L, "최신 안드로이드 스마트폰", 50));
-        products.add(new Product("iPhone 16", 1350000L, "Apple의 최신 스마트폰", 30));
-        products.add(new Product("MacBook Pro", 2400000L, "M3 칩셋이 탑재된 노트북", 15));
-        products.add(new Product("AirPods Pro", 350000L, "노이즈 캔슬링 무선 이어폰", 50));
+        
+        List<Product> electronicsProducts = new ArrayList<>();
+        electronicsProducts.add(new Product("Galaxy S24", 1200000L, "최신 안드로이드 스마트폰", 50));
+        electronicsProducts.add(new Product("iPhone 15", 1350000L, "Apple의 최신 스마트폰", 30));
+        electronicsProducts.add(new Product("MacBook Pro", 2400000L, "M3 칩셋이 탑재된 노트북", 15));
+        electronicsProducts.add(new Product("AirPods Pro", 350000L, "노이즈 캔슬링 무선 이어폰", 50));
 
-        CommerceSystem commerceSystem = new CommerceSystem(products);
-        commerceSystem.start();
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category("전자제품", electronicsProducts));
+        categories.add(new Category("의류", new ArrayList<>()));
+        categories.add(new Category("식품", new ArrayList<>()));
+
+        // CommerceSystem 내 시작하는 함수 호출
+        CommerceSystem system = new CommerceSystem(categories);
+        system.start();
     }
 }
